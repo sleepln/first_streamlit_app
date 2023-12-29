420c7638d89778849ad82a4b620b5cca8d319cd7
sleepln <155114964+sleepln@users.noreply.github.com>
2023-12-28 17:26:02 -0700

    Update streamlit_app.py
    show fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#display the table on the page
+streamlit.dataframe(fruits_to_show)
