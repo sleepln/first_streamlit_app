5465e50234634a52b285dadf85e0b63278557d4a
sleepln <155114964+sleepln@users.noreply.github.com>
2023-12-31 12:33:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text ('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -45,7 +45,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-import requests
+#import requests
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
